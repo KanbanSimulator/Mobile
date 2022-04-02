abc66caaadfeab06e258adfc283af052365f4c52
Timur Nugaev <decter49pro@gmail.com>
2022-04-03 00:51:49 +0300

    refactor

--- lib/widget/task_table.dart
+++ lib/widget/task_table.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:kanban/api/api_basic.dart';
-import 'package:kanban/const/app_style.dart';
+import 'package:kanban/api/api.dart';
 import 'package:kanban/model/task_model.dart';
 import 'package:kanban/page/error.dart';
 import 'package:kanban/widget/task_card.dart';

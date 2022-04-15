3528781f381e76a4adb006bbff4af5d9686ace06
Timur Nugaev <decter49pro@gmail.com>
2022-04-15 14:30:54 +0300

    split task column and task table widgets into separate files for maintainability

---
+++ lib/widget/task_column.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:kanban/model/task_model.dart';
+import 'package:kanban/widget/task_card.dart';
+
+class TaskColumn extends StatelessWidget {
+  final List<TaskModel> tasks;
+
+  const TaskColumn({
+    Key? key,
+    required this.tasks,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          Widget taskCardWidget;
+          taskCardWidget = TaskCard(
+            taskModel: tasks[index],
+          );
+          return Draggable(
+            // delay: Duration(seconds: 2), // for mobile use longdraggable
+            child: taskCardWidget,
+            childWhenDragging: const Text("widget was here."),
+            feedback: Card(
+              color: Colors.transparent,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 6 - 16,
+                height: 138,
+                child: taskCardWidget,
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            height: 4,
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/widget/task_table.dart
+++ lib/widget/task_table.dart
@@ -5,6 +5,7 @@ import 'package:kanban/core/api/api.dart';
 import 'package:kanban/model/task_model.dart';
 import 'package:kanban/page/error.dart';
 import 'package:kanban/widget/task_card.dart';
+import 'package:kanban/widget/task_column.dart';
 
 class TaskTable extends StatelessWidget {
   final int day;
@@ -61,75 +62,7 @@ class TaskTable extends StatelessWidget {
             ),
           ],
         );
-
-        // return Expanded(
-        //   child: ListView.builder(
-        //     itemCount: tasks!.length,
-        //     itemBuilder: (context, index) {
-        //       return TaskCard(taskModel: tasks[index]);
-        //     },
-        //   ),
-        // );
-
-        // return Expanded(
-        //   child: GridView.count(
-        //     physics: const BouncingScrollPhysics(),
-        //     crossAxisCount: 3,
-        //     mainAxisSpacing: 5,
-        //     crossAxisSpacing: 12.5,
-        //     children: List.generate(tasks!.length, (index) {
-        //       return TaskCard(
-        //         taskModel: tasks[index],
-        //       );
-        //     }),
-        //   ),
-        // );
       },
-    );
-  }
-}
-
-class TaskColumn extends StatelessWidget {
-  final List<TaskModel> tasks;
-
-  const TaskColumn({
-    Key? key,
-    required this.tasks,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: ListView.separated(
-        physics: const BouncingScrollPhysics(),
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          final rng = Random();
-          Widget taskCardWidget;
-          taskCardWidget = TaskCard(
-            taskModel: tasks[index],
-          );
-          return Draggable(
-            // delay: Duration(seconds: 2), // for mobile use longdraggable
-            child: taskCardWidget,
-            childWhenDragging: const Text("widget was here."),
-            feedback: Card(
-              color: Colors.transparent,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 6 - 16,
-                height: 138,
-                child: taskCardWidget,
-              ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(
-            height: 4,
-          );
-        },
-      ),
     );
   }
 }

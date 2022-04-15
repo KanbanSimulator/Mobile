ad13fa2b9841c001fac539b1d12365dc0f4f54a7
Timur Nugaev <decter49pro@gmail.com>
2022-04-15 14:37:20 +0300

    update column colors so that it is easier to see

--- lib/widget/task_column.dart
+++ lib/widget/task_column.dart
@@ -14,33 +14,38 @@ class TaskColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: ListView.separated(
-        physics: const BouncingScrollPhysics(),
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          Widget taskCardWidget;
-          taskCardWidget = TaskCard(
-            taskModel: tasks[index],
-          );
-          return Draggable(
-            // delay: Duration(seconds: 2), // for mobile use longdraggable
-            child: taskCardWidget,
-            childWhenDragging: const Text("widget was here."),
-            feedback: Card(
-              color: Colors.transparent,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width / 6 - 16,
-                height: 138,
-                child: taskCardWidget,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xff151515),
+        ),
+        child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            Widget taskCardWidget;
+            taskCardWidget = TaskCard(
+              taskModel: tasks[index],
+            );
+            return Draggable(
+              // delay: Duration(seconds: 2), // for mobile use longdraggable
+              child: taskCardWidget,
+              childWhenDragging: const Text("widget was here."),
+              feedback: Card(
+                color: Colors.transparent,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width / 6 - 16,
+                  height: 138,
+                  child: taskCardWidget,
+                ),
               ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(
-            height: 4,
-          );
-        },
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(
+              height: 4,
+            );
+          },
+        ),
       ),
     );
   }
